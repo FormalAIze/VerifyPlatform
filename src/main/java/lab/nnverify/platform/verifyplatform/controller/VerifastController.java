3b07248334a168efc200446f4d526ea2e165c54b
makise <10175101106@stu.ecnu.edu.cn>
2021-03-19 13:01:23 +0800

    feat: add websocket dependency and rename controller

--- src/main/java/lab/nnverify/platform/verifyplatform/controller/VerifastController.java
+++ src/main/java/lab/nnverify/platform/verifyplatform/controller/VerifastController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class Verifast {
+public class VerifastController {
 
     @RequestMapping("/verifast/mipverify")
     public void VerifastUseMIPVerify() {
