9cf78ebae584a387998cebea9fe81c2dbc5310c5
makise <10175101106@stu.ecnu.edu.cn>
2021-07-10 20:59:34 +0800

    feat: user service connect with mysql on server

--- src/main/java/lab/nnverify/platform/verifyplatform/services/MyUserDetailsService.java
+++ src/main/java/lab/nnverify/platform/verifyplatform/services/MyUserDetailsService.java
@@ -1,5 +1,8 @@
 package lab.nnverify.platform.verifyplatform.services;
 
+import lab.nnverify.platform.verifyplatform.mapper.UserMapper;
+import lab.nnverify.platform.verifyplatform.models.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,8 +13,12 @@ import java.util.ArrayList;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new User("foo", "foo", new ArrayList<>());
+        UserModel user = userMapper.fetchUserByName(s);
+        return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
     }
 }
