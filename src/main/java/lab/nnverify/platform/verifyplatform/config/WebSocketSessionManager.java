6a489b0bb3859f075f6ccd44f7cd09c09940bea8
makise <10175101106@stu.ecnu.edu.cn>
2021-07-21 01:15:08 +0800

    feat: make session pool concurrent

--- src/main/java/lab/nnverify/platform/verifyplatform/config/WebSocketSessionManager.java
+++ src/main/java/lab/nnverify/platform/verifyplatform/config/WebSocketSessionManager.java
@@ -1,19 +1,21 @@
 package lab.nnverify.platform.verifyplatform.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 public class WebSocketSessionManager {
-    private static HashMap<String, WebSocketSession> sessionPool = new HashMap<>();
+    private static final Map<String, WebSocketSession> sessionPool = new ConcurrentHashMap<>();
 
     public static void addSession(String id, WebSocketSession session) {
         sessionPool.put(id, session);
-        log.info("session add to session pool, uri: " + session.getUri());
+        log.info("session add to session pool, uri: " + session.getUri() + " userId: " + id);
+        log.info("current session count: " + sessionPool.size());
+        log.info("sessionPool: " + sessionPool.toString());
     }
 
     public static void deleteSession(WebSocketSession session) {
