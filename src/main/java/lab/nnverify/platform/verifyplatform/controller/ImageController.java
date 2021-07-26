1488c9a2b5e136dc47db9d3c9bba4b1cee6cce53
makise <10175101106@stu.ecnu.edu.cn>
2021-07-26 15:09:01 +0800

    feat: upload basic function finished

--- src/main/java/lab/nnverify/platform/verifyplatform/controller/ImageController.java
+++ src/main/java/lab/nnverify/platform/verifyplatform/controller/ImageController.java
@@ -1,7 +1,10 @@
 package lab.nnverify.platform.verifyplatform.controller;
 
+import lab.nnverify.platform.verifyplatform.models.ResponseEntity;
+import lab.nnverify.platform.verifyplatform.services.ImageService;
 import lab.nnverify.platform.verifyplatform.verifykit.winr.WiNRConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -10,23 +13,26 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @Controller
 public class ImageController {
+    @Autowired
+    ImageService imageService;
+
     private final String wiNRBasePath = WiNRConfig.basicPath + "adv_examples/";
+
     @Value("${winr.file.upload.url}")
     private String uploadFilepathWiNR;
+
+    @Value("${deepcert.file.upload.url}")
+    private String uploadFilepathDeepcert;
 
     @CrossOrigin(origins = "*")
     @GetMapping(value = "/winr/adv_image/{filename}", produces = MediaType.IMAGE_PNG_VALUE)
     @ResponseBody
     public byte[] wiNRAdvExample(@PathVariable String filename) throws Exception {
-
         File file = new File(wiNRBasePath + filename);
         FileInputStream inputStream = new FileInputStream(file);
         byte[] bytes = new byte[inputStream.available()];
@@ -37,28 +43,60 @@ public class ImageController {
     @CrossOrigin(origins = "*")
     @PostMapping("/winr/images")
     @ResponseBody
-    public List<String> wiNRImageUpload(@RequestParam("images") MultipartFile[] images) {
+    public ResponseEntity wiNRImageUpload(@RequestParam("images") MultipartFile[] images) {
         ArrayList<String> imageNames = new ArrayList<>();
+        ResponseEntity response = new ResponseEntity();
+        int successCount = 0;
+        int imageCount = images.length;
+        if (imageCount == 0) {
+            response.setStatus(-410);
+            response.setMsg("no image uploaded");
+            return response;
+        }
         for (MultipartFile image : images) {
-            String filename = image.getOriginalFilename();
-            log.info(filename);
-            int idx = filename != null ? filename.lastIndexOf(".") : 0;
-            String extension = filename.substring(idx);
-            String uuidFileName = UUID.randomUUID().toString().replace("-", "") + extension;
-            String destPath = uploadFilepathWiNR + "/" + uuidFileName;
-            log.info("destPath: " + destPath);
-            File dest = new File(destPath);
-            if (!dest.getParentFile().exists()) {
-                dest.getParentFile().mkdirs();
-            }
-            try {
-                image.transferTo(dest);
+            String uuidFileName = imageService.generateUUidFilename(image);
+            String destPath = uploadFilepathDeepcert + "/" + uuidFileName;
+            if (imageService.saveImage(image, destPath)) {
                 imageNames.add(destPath);
-            } catch (IOException e) {
-                e.printStackTrace();
-                log.error("文件" + destPath + "上传保存失败");
+                successCount++;
+            } else {
+                log.error("iamge save failed, filename is: " + destPath);
             }
         }
-        return imageNames;
+        response.getData().put("imageNames", imageNames);
+        response.setStatus(successCount == 0 ? -510 : 200);
+        response.getData().put("successSave", successCount);
+        response.getData().put("imageUpload", imageCount);
+        return response;
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/deepcert/images")
+    @ResponseBody
+    public ResponseEntity deepCertImageUpload(@RequestParam("images") MultipartFile[] images) {
+        ArrayList<String> imageNames = new ArrayList<>();
+        ResponseEntity response = new ResponseEntity();
+        int successCount = 0;
+        int imageCount = images.length;
+        if (imageCount == 0) {
+            response.setStatus(-410);
+            response.setMsg("no image uploaded");
+            return response;
+        }
+        for (MultipartFile image : images) {
+            String uuidFileName = imageService.generateUUidFilename(image);
+            String destPath = uploadFilepathWiNR + "/" + uuidFileName;
+            if (imageService.saveImage(image, destPath)) {
+                imageNames.add(destPath);
+                successCount++;
+            } else {
+                log.error("iamge save failed, filename is: " + destPath);
+            }
+        }
+        response.getData().put("imageNames", imageNames);
+        response.setStatus(successCount == 0 ? -510 : 200);
+        response.getData().put("successSave", successCount);
+        response.getData().put("imageUpload", imageCount);
+        return response;
     }
 }
---
+++ src/main/java/lab/nnverify/platform/verifyplatform/services/ImageService.java
@@ -0,0 +1,43 @@
+package lab.nnverify.platform.verifyplatform.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class ImageService {
+    public boolean saveImage(MultipartFile image, String path) {
+        log.info("the save path is: " + path);
+        File dest = new File(path);
+        if (!dest.getParentFile().exists()) {
+            if (!dest.getParentFile().mkdirs()) {
+                log.error("mkdirs files, path: " + path);
+                return false;
+            }
+        }
+        try {
+            image.transferTo(dest);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("文件" + path + "上传保存失败");
+            return false;
+        }
+    }
+
+    public String generateUUidFilename(MultipartFile file) {
+        String filename = file.getOriginalFilename();
+        if (filename == null) {
+            return "";
+        }
+        log.info("origin filename: " + filename);
+        int idx = filename.lastIndexOf(".");
+        String extension = filename.substring(idx);
+        return UUID.randomUUID().toString().replace("-", "") + extension;
+    }
+}
